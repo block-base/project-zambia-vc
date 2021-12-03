b4203bce6ca90f5c5533eac2f530cf8bea8bde6f
Taiju Sanagi <taijusanagi@Taijus-MacBook-Air.local>
2021-12-03 09:52:48 +0900

    chore: change schema to HMAC_SHA_256 adjust to GAS

--- packages/backend/src/middleware/auth/hmac.ts
+++ packages/backend/src/middleware/auth/hmac.ts
@@ -10,18 +10,12 @@ export const hmacAuthMiddleWare: RequestHandler = (req, res, next) => {
     throw new Error("authorization header not set");
   }
   const [, schema, signature] = headers.authorization.split(" ");
-  if (schema !== "HMAC-SHA256") {
+  if (schema !== "HMAC_SHA_256") {
     throw new Error("authorization schema invalid");
   }
   const contentString = JSON.stringify(body, Object.keys(body).sort());
-  const contentHash = crypto
-    .createHash("sha256")
-    .update(contentString)
-    .digest("base64");
-  const expectedSignature = crypto
-    .createHmac("sha256", env.authSecret)
-    .update(contentHash)
-    .digest("base64");
+  const contentHash = crypto.createHash("sha256").update(contentString).digest("base64");
+  const expectedSignature = crypto.createHmac("sha256", env.authSecret).update(contentHash).digest("base64");
   if (signature !== expectedSignature) {
     throw new Error("hmac signature invalid");
   }
